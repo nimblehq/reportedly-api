d883ffd66d559af1ee0a79805bfae025853ef69e
Noor Ahmed <biswas.noorahmed@gmail.com>
2020-12-10 10:11:03 +0600

    [task-7] Adds report submisson, report list APIs

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -3,14 +3,33 @@
 module Api
   module V1
     class ReportsController < Api::V1::BaseController
-      def create
+      def show
+        render json: {
+          status: { message: 'User Reports.' },
+          data: ReportSerializer.new(current_user_reports, is_collection: true)
+        }
+      end
 
+      def create
+        report = Report.new(report_params)
+
+        if report.save
+          render json: {
+            status: { message: 'Report submitted, Thanks' }
+          }
+        else
+          render_error(detail: report.errors.full_messages.to_sentence)
+        end
       end
 
       private
 
       def report_params
-        params.required(:report).permit(:user, :task_today, :obstacles_today)
+        params.required(:report).permit(:user_id, :task_today, :obstacles_today)
+      end
+
+      def current_user_reports
+        current_user.reports.last(5).reverse
       end
     end
   end
