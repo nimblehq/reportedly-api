085ab2ca3b4f536e9023e117d0e336ee63e2bb96
Noor Ahmed <biswas.noorahmed@gmail.com>
2020-12-11 10:44:46 +0600

    [task-6] fixes rubocop issues

--- app/services/slack_notification_service.rb
+++ app/services/slack_notification_service.rb
@@ -11,15 +11,15 @@ class SlackNotificationService
 
       req_options = { use_ssl: uri.scheme == 'https' }
 
-      if SlackThread.find_by(created_on: Time.zone.today).nil?
-        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-          http.request(set_request_body(uri))
-        end
+      return unless SlackThread.find_by(created_on: Time.zone.today).nil?
 
-        result = JSON.parse(response.body)
-
-        SlackThread.create(thread_ts: result['ts'], created_on: Time.zone.today)
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(set_request_body(uri))
       end
+
+      result = JSON.parse(response.body)
+
+      SlackThread.create(thread_ts: result['ts'], created_on: Time.zone.today)
     end
 
     private
@@ -29,8 +29,8 @@ class SlackNotificationService
       request.content_type = 'application/json'
       request['Authorization'] = 'Bearer xoxb-1289907288471-1577816759697-unrSMHhgnn5EocXsOHmqBynn'
       request.body = JSON.dump({
-                                 "channel" => "C01GZQ5T589",
-                                 "text" => "Here are the results of Nimble - Daily Standup from #{Time.now.to_date}"
+                                 'channel' => 'C01GZQ5T589',
+                                 'text' => "Here are the results of Nimble - Daily Standup on #{Time.zone.now.to_date}"
                                })
       request
     end
